f4f243cb695c6d192d097497c043876c6e2231bf
rhevaldi <rhevaldy8889@gmail.com>
2026-08-18 23:42:01 +0800

    fix: resolve php auto download issue on vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
 
+// Direct request file statis jika ada
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    return false;
+}
+
+// Teruskan request utama ke public/index.php Laravel
 require __DIR__ . '/../public/index.php';
